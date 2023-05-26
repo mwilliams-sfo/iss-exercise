d11d3c1d17b54980dcf83baa53b0ee451e0791b3
Myles Williams <mwilliams.sfo@gmail.com>
2023-05-25 20:02:51 -0700

    Initial commit

---
+++ app/src/main/java/com/example/chase/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.chase
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.chase.ui.theme.ChaseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
